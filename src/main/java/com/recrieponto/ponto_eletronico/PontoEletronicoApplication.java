e69b4d465e4d46db890c325bb97ef670c24163e4
leonardo <leonardo.dias3@estudante.ifms.edu.br>
2025-10-12 00:04:15 -0300

    Adiciona logs de depuração para variáveis de ambiente

--- src/main/java/com/recrieponto/ponto_eletronico/PontoEletronicoApplication.java
+++ src/main/java/com/recrieponto/ponto_eletronico/PontoEletronicoApplication.java
@@ -6,12 +6,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder; // <-- IMPORT
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PontoEletronicoApplication {
 
     public static void main(String[] args) {
+        // --- INÍCIO DO NOSSO ESPIÃO ---
+        // Vamos imprimir os valores das variáveis de ambiente que a aplicação está "enxergando".
+        System.out.println("!!!!!!!!!!!!!!!!!! INICIANDO TESTE DE AMBIENTE !!!!!!!!!!!!!!!!!!");
+        System.out.println("!!! LENDO VARIAVEL SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("!!! LENDO VARIAVEL SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("!!! LENDO VARIAVEL SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        // --- FIM DO ESPIÃO ---
+
         SpringApplication.run(PontoEletronicoApplication.class, args);
     }
 
@@ -19,17 +28,10 @@ public class PontoEletronicoApplication {
     CommandLineRunner initDatabase(CoordenadorRepository repository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (repository.findByUsername("admingeral").isEmpty()) {
-                System.out.println(">>> Criando usuário admin padrão (COM SENHA CRIPTOGRAFADA)...");
-
-                // Criptografa a senha aqui mesmo
+                System.out.println(">>> Criando usuário admin padrão...");
                 String senhaCriptografada = passwordEncoder.encode("admin123");
-
-                // Cria o objeto com a senha já criptografada
                 Coordenador admin = new Coordenador("admingeral", senhaCriptografada, "ADMIN");
-
-                // Salva diretamente no banco
                 repository.save(admin);
-
                 System.out.println(">>> Usuário admin criado.");
             }
         };
